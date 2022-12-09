3e4152afe6c60999d261b76a6bfa73645d4240ab
gotwarlost <krishnan@the-krishnans.com>
2022-12-08 23:33:02 -0800

    testificate

--- dec9/puzzle_test.go
+++ dec9/puzzle_test.go
@@ -10,6 +10,9 @@ import (
 //go:embed test-input.txt
 var testInput string
 
+//go:embed test-input2.txt
+var testInput2 string
+
 func TestP1(t *testing.T) {
 	assert.Equal(t, 13, countVisited(testInput, 2))
 	assert.Equal(t, 5695, countVisited(input, 2))
@@ -17,5 +20,6 @@ func TestP1(t *testing.T) {
 
 func TestP2(t *testing.T) {
 	assert.Equal(t, 1, countVisited(testInput, 10))
+	assert.Equal(t, 36, countVisited(testInput2, 10))
 	assert.Equal(t, 2434, countVisited(input, 10))
 }
