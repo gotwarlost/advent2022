9859d88d9c3118e2a14463ff2dbbdce1611b41dc
gotwarlost <krishnan@the-krishnans.com>
2022-12-09 21:49:30 -0800

    dec10

--- dec10/puzzle.go
+++ dec10/puzzle.go
@@ -3,13 +3,85 @@ package dec10
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+type strengths []int
+
+func (s strengths) multAt(n int) int {
+	return n * s[n-1]
+}
+
+func (s strengths) valueAt(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return s[n-1]
+}
+
+type signal struct {
+	xInc int
+}
 
 //go:embed input.txt
 var input string
 
-func run(in string) int {
-	return 0
+func toSignals(in string) []signal {
+	var sigs []signal
+	lines := strings.Split(strings.TrimSpace(in), "\n")
+	for _, line := range lines {
+		switch line {
+		case "noop":
+			sigs = append(sigs, signal{})
+		default:
+			parts := strings.Split(line, " ")
+			if len(parts) != 2 || parts[0] != "addx" {
+				panic(line)
+			}
+			n, err := strconv.Atoi(parts[1])
+			if err != nil {
+				panic(err)
+			}
+			sigs = append(sigs, signal{}, signal{xInc: n})
+		}
+	}
+	return sigs
+}
+
+func run(in string) strengths {
+	sigs := toSignals(in)
+	x := 1
+	var afterValues []int
+	for _, cycle := range sigs {
+		x += cycle.xInc
+		afterValues = append(afterValues, x)
+	}
+	return afterValues
+}
+
+func run2(in string) {
+	ss := run(in)
+	cycle := 0
+	for row := 0; row < 6; row++ {
+		for col := 0; col < 40; col++ {
+			var s int
+			if cycle == 0 {
+				s = 1
+			} else {
+				s = ss.valueAt(cycle)
+			}
+			min := s - 1
+			max := s + 1
+			if col >= min && col <= max {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+			cycle++
+		}
+		fmt.Println("")
+	}
 }
 
 func RunP1() {
@@ -17,5 +89,5 @@ func RunP1() {
 }
 
 func RunP2() {
-	fmt.Println(run(input))
+	run2(input)
 }
--- dec10/puzzle_test.go
+++ dec10/puzzle_test.go
@@ -2,6 +2,7 @@ package dec10
 
 import (
 	_ "embed"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,6 +14,18 @@ var _ = assert.Equal
 var testInput string
 
 func TestP1(t *testing.T) {
+	s := run(testInput)
+	log.Println("S=", s[19], s[59], s[99], s[139], s[179], s[219])
+	assert.EqualValues(t, 420, s.multAt(20))
+	assert.EqualValues(t, 1140, s.multAt(60))
+	assert.EqualValues(t, 1800, s.multAt(100))
+	assert.EqualValues(t, 2940, s.multAt(140))
+	assert.EqualValues(t, 2880, s.multAt(180))
+	assert.EqualValues(t, 3960, s.multAt(220))
+	assert.Equal(t, 13140, s.multAt(20)+s.multAt(60)+s.multAt(100)+s.multAt(140)+s.multAt(180)+s.multAt(220))
+
+	s = run(input)
+	assert.Equal(t, 17940, s.multAt(20)+s.multAt(60)+s.multAt(100)+s.multAt(140)+s.multAt(180)+s.multAt(220))
 }
 
 func TestP2(t *testing.T) {
