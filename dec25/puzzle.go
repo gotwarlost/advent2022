057cb4e5f0097875850a15bbc1d568f7a3095b79
gotwarlost <krishnan@the-krishnans.com>
2022-12-25 09:40:08 -0800

    day 25 and done

--- dec25/puzzle.go
+++ dec25/puzzle.go
@@ -3,13 +3,78 @@ package dec25
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 )
 
 //go:embed input.txt
 var input string
 
-func runP1(in string) int {
-	return 9
+func snafuToDecimal(s string) int64 {
+	sum := int64(0)
+	mult := int64(1)
+	for i := len(s) - 1; i >= 0; i-- {
+		x := s[i]
+		var val int
+		switch x {
+		case '=':
+			val = -2
+		case '-':
+			val = -1
+		case '0':
+			val = 0
+		case '1':
+			val = 1
+		case '2':
+			val = 2
+		default:
+			panic(fmt.Errorf("invalid SNAFU digit: %v", x))
+		}
+		sum += int64(val) * mult
+		mult *= 5
+	}
+	return sum
+}
+
+func decimalToSnafu(x int64) string {
+	var ret []byte
+	prepend := func(b byte) {
+		ret = append([]byte{b}, ret...)
+	}
+	for {
+		q := x / int64(5)
+		r := x % int64(5)
+		switch r {
+		case 0:
+			prepend('0')
+		case 1:
+			prepend('1')
+		case 2:
+			prepend('2')
+		case 3:
+			q++
+			prepend('=')
+		case 4:
+			q++
+			prepend('-')
+		}
+		x = q
+		if x == 0 {
+			break
+		}
+	}
+	return string(ret)
+}
+
+func toSnafus(in string) []string {
+	return strings.Split(strings.TrimSpace(in), "\n")
+}
+
+func runP1(in string) string {
+	total := int64(0)
+	for _, s := range toSnafus(in) {
+		total += snafuToDecimal(s)
+	}
+	return decimalToSnafu(total)
 }
 
 func runP2(in string) int {
--- dec25/puzzle_test.go
+++ dec25/puzzle_test.go
@@ -2,6 +2,7 @@ package dec25
 
 import (
 	_ "embed"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,9 +13,37 @@ var _ = assert.Equal
 //go:embed test-input.txt
 var testInput string
 
+func TestSnafuToDecimal(t *testing.T) {
+	tests := []struct {
+		snafu    string
+		expected int
+	}{
+		{"1=-0-2", 1747},
+		{"12111", 906},
+		{"2=0=", 198},
+		{"21", 11},
+		{"2=01", 201},
+		{"111", 31},
+		{"20012", 1257},
+		{"112", 32},
+		{"1=-1=", 353},
+		{"1-12", 107},
+		{"12", 7},
+		{"1=", 3},
+		{"122", 37},
+		{"1=====--10201111-0", 381535316395},
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test-%d", i+1), func(t *testing.T) {
+			assert.EqualValues(t, test.expected, snafuToDecimal(test.snafu))
+			assert.EqualValues(t, test.snafu, decimalToSnafu(int64(test.expected)))
+		})
+	}
+}
+
 func TestP1(t *testing.T) {
-	assert.EqualValues(t, 110, runP1(testInput))
-	// assert.EqualValues(t, 3882, runP1(input))
+	assert.EqualValues(t, "2=-1=0", runP1(testInput))
+	assert.EqualValues(t, "2-00=12=21-0=01--000", runP1(input))
 }
 
 func TestP2(t *testing.T) {
